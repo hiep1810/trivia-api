6e4a84d7390b8e46acfb878b8b82c1dde8b7fd44
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2023-11-27 13:43:11 +0700

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
-from settings import DB_NAME, DB_USER, DB_PASSWORD
+from settings import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -16,7 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = DB_NAME
-        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD, 'localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD, DB_HOST, self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
